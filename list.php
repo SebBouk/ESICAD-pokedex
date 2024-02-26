8dcee95ba83d051769b79f5f0dd6d84d82dc23c7
Sébastien <seb.boukobza@gmail.com>
2024-02-26 15:22:27 +0100

    rajout type

--- list.php
+++ list.php
@@ -4,21 +4,32 @@
 <?php
 require_once("head.php");
 ?>
-<table>
-    <thead>
-        <th>Numéro</th>
-        <th>Nom</th>
-        <th>Photo</th>
+<table class = "tableau_pokemon">
+    <thead >
+        <th class = "tableau_all">Numéro</th>
+        <th class = "tableau_all">Nom</th>
+        <th class = "tableau_all">Photo</th>
+        <th class = "tableau_all">PV</th>
+        <th class = "tableau_all">Attaque</th>
+        <th class = "tableau_all">Defense</th>
+        <th class = "tableau_all">Vitesse</th>
+        <th class = "tableau_all">Special</th>
+        <th class = "tableau_all">Type 1</th>
+        <th class = "tableau_all">Type 2</th>
 <?php
 require_once("database-connection.php");
-$query = $databaseConnection->query("SELECT * from pokemon");
+$query = $databaseConnection->query("SELECT pokemon.*, type1.libelleType AS premier_type, type2.libelleType AS deuxieme_type
+FROM pokemon
+JOIN typepokemon AS type1 ON type1.IdType = pokemon.IdTypePokemon
+JOIN typepokemon AS type2 ON type2.IdType = pokemon.IdSecondTypePokemon
+ORDER BY IdPokemon ASC;");
 
 if (!$query) {
     throw new RuntimeException ("Cannot execute query. Cause : " . mysqli_error($connection)); 
 } else {
     $pokemons = $query->fetch_all(MYSQLI_ASSOC);
     foreach ($pokemons as $pokemon) {
-        echo "<tr><td>" . $pokemon["IdPokemon"] . "</td><td>" . $pokemon["NomPokemon"] . "</td><td><img src='" . $pokemon["urlPhoto"] . "'></td></tr>";
+        echo "<tr><td class = tableau_all>" . $pokemon["IdPokemon"] . "</td><td class = tableau_all>" . $pokemon["NomPokemon"] . "</td><td class = tableau_all><img src='" . $pokemon["urlPhoto"] . "'></td><td class = tableau_all>" . $pokemon["PV"] . "</td><td class = tableau_all>" . $pokemon["Attaque"] . "</td><td class = tableau_all>" . $pokemon["Defense"] . "</td><td class = tableau_all>" . $pokemon["Vitesse"] . "</td><td class = tableau_all>" . $pokemon["Special"] . "</td><td class = tableau_all>" . $pokemon["premier_type"] . "</td><td class = tableau_all>" . $pokemon["deuxieme_type"] . "</td></tr>";
     }
 }
 ?>
