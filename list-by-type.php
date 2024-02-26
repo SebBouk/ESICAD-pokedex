3e737262241d8f56f9d85bcc7fa4b66c3785367f
Sébastien <seb.boukobza@gmail.com>
2024-02-26 18:37:48 +0100

    essai retour simple

--- list-by-type.php
+++ list-by-type.php
@@ -8,7 +8,7 @@ require_once("head.php");
 require_once("database-connection.php");
 
 // Requête SQL pour récupérer les types de Pokémon
-$sql_types = "SELECT libelleType FROM typepokemon";
+$sql_types = "SELECT libelleType FROM typepokemon ORDER BY IdType";
 $result_types = $databaseConnection->query($sql_types);
 
 // Afficher les boutons pour chaque type de Pokémon
@@ -33,16 +33,17 @@ if(isset($_GET['type'])) {
     // Afficher la liste des Pokémon correspondants
     if ($result->num_rows > 0) {
         echo "<h2>Pokémon de type $type :</h2>";
-        echo "<thead>";
+        echo "<table>";
         echo "<th class = tableau_all>Numéro</th>
         <th class = tableau_all>Nom</th>
-        <th class = tableau_all>Photo</th>
-        <th class = tableau_all>Type 1</th>
-        <th class = tableau_all>Type 2</th>";
+        <th class = tableau_all>Photo</th>";
         while($row = $result->fetch_assoc()) {
-            echo "<tr><td class = tableau_all>" . $row["IdPokemon"] . "</td><td class = tableau_all>" . $row["NomPokemon"] . "</td><td class = tableau_all><img src='" . $row["urlPhoto"] . "'></td><td class = tableau_all>" . $row["libelleType"] . "</td><td class = tableau_all>" . $row["libelleType"] . "</td></tr>";
+            echo "<tr><td class = tableau_all>" . $row["IdPokemon"] . 
+            "</td><td class = tableau_all>" . $row["NomPokemon"] . 
+            "</td><td class = tableau_all><img src='" . $row["urlPhoto"] . 
+            "'></td></tr>";
         }
-        echo "</thead>";
+        echo "</table>";
     } else {
         echo "Aucun Pokémon trouvé pour ce type";
     }
